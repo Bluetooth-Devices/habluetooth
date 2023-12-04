6fb3842997011c436d8f1c31ee5279d5417ddb7d
J. Nick Koston <nick@koston.org>
2023-12-04 09:52:31 -1000

    fix: adjust

--- src/habluetooth/__init__.py
+++ src/habluetooth/__init__.py
@@ -1,6 +1,9 @@
 __version__ = "0.5.1"
 
-from .advertisement_tracker import AdvertisementTracker
+from .advertisement_tracker import (
+    TRACKER_BUFFERING_WOBBLE_SECONDS,
+    AdvertisementTracker,
+)
 from .base_scanner import BaseHaRemoteScanner, BaseHaScanner
 from .const import (
     CONNECTABLE_FALLBACK_MAXIMUM_STALE_ADVERTISEMENT_SECONDS,
@@ -12,6 +15,7 @@ from .models import HaBluetoothConnector
 from .scanner import BluetoothScanningMode, HaScanner, ScannerStartError
 
 __all__ = [
+    "TRACKER_BUFFERING_WOBBLE_SECONDS",
     "AdvertisementTracker",
     "BluetoothScanningMode",
     "ScannerStartError",
