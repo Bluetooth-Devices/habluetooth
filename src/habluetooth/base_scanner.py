40ef15199e3bf094d0d347b4b90f7fff04294797
J. Nick Koston <nick@koston.org>
2025-03-22 21:50:55 -1000

    feat: optimize _dict_subset

--- src/habluetooth/base_scanner.py
+++ src/habluetooth/base_scanner.py
@@ -39,8 +39,12 @@ _str = str
 
 def _dict_subset(super_dict: dict[Any, bytes], sub_dict: dict[Any, bytes]) -> bool:
     """Return True if sub_dict is a subset of super_dict."""
-    for key, sub_value in sub_dict.items():
-        if (super_value := super_dict.get(key)) is None or super_value != sub_value:
+    for key in sub_dict:
+        if (value := super_dict.get(key)) is None:
+            return False
+        super_value = value
+        sub_value = sub_dict[key]
+        if super_value != sub_value:
             return False
     return True
 
