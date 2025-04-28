bb78d7151c617156c0b4879bbcccc94aaa2711a2
J. Nick Koston <nick@koston.org>
2025-04-28 14:32:07 -0400

    feat: add bt management side channel

---
+++ src/habluetooth/channels/bluez.pxd
@@ -0,0 +1,35 @@
+
+import cython
+
+
+cdef bint TYPE_CHECKING
+
+
+cdef class BluetoothMGMTProtocol:
+
+    cdef object transport
+    cdef object connection_mode_future
+    cdef bytes _buffer
+    cdef unsigned int _buffer_len
+    cdef unsigned int _pos
+
+    @cython.locals(
+        original_pos="unsigned int",
+        new_pos="unsigned int",
+        cstr="const unsigned char *"
+    )
+    cdef bytes _read(self, int length)
+
+    @cython.locals(bytes_data=bytes)
+    cdef void _add_to_buffer(self, object data) except *
+
+    @cython.locals(end_of_frame_pos="unsigned int", cstr="const unsigned char *")
+    cdef void _remove_from_buffer(self) except *
+
+    @cython.locals(
+        header="const unsigned char *",
+        event_code="unsigned short",
+        controller_idx="unsigned short",
+        param_len="unsigned short"
+    )
+    cpdef void data_received(self, object data) except *
--- src/habluetooth/channels/bluez.py
+++ src/habluetooth/channels/bluez.py
@@ -4,25 +4,29 @@ import asyncio
 import logging
 import socket
 from asyncio import timeout as asyncio_timeout
-from typing import Any, cast
+from typing import TYPE_CHECKING, cast
 
 from btsocket import btmgmt_protocol, btmgmt_socket
 
 _LOGGER = logging.getLogger(__name__)
+_int = int
+
+# Everything is little endian
+
+HEADER_SIZE = 6
+# Header is event_code (2 bytes), controller_idx (2 bytes), param_len (2 bytes)
 
 
-class BluetoothMGMTProtocol(asyncio.Protocol):
+class BluetoothMGMTProtocol:
     """Bluetooth MGMT protocol."""
 
-    def __init__(
-        self, timeout: float, connection_mode_future: asyncio.Future[None]
-    ) -> None:
+    def __init__(self, connection_mode_future: asyncio.Future[None]) -> None:
         """Initialize the protocol."""
-        self.future: asyncio.Future[btmgmt_protocol.Response] | None = None
         self.transport: asyncio.Transport | None = None
-        self.timeout = timeout
         self.connection_mode_future = connection_mode_future
-        self.loop = asyncio.get_running_loop()
+        self._buffer: bytes | None = None
+        self._buffer_len = 0
+        self._pos = 0
 
     def connection_made(self, transport: asyncio.BaseTransport) -> None:
         """Handle connection made."""
@@ -30,36 +34,78 @@ class BluetoothMGMTProtocol(asyncio.Protocol):
             self.connection_mode_future.set_result(None)
         self.transport = cast(asyncio.Transport, transport)
 
+    def _add_to_buffer(self, data: bytes | bytearray | memoryview) -> None:
+        """Add data to the buffer."""
+        # Protractor sends a bytearray, so we need to convert it to bytes
+        # https://github.com/esphome/issues/issues/5117
+        # type(data) should not be isinstance(data, bytes) because we want to
+        # to explicitly check for bytes and not for subclasses of bytes
+        bytes_data = bytes(data) if type(data) is not bytes else data
+        if self._buffer_len == 0:
+            # This is the best case scenario, we don't have to copy the data
+            # and can just use the buffer directly. This is the most common
+            # case as well.
+            self._buffer = bytes_data
+        else:
+            if TYPE_CHECKING:
+                assert self._buffer is not None, "Buffer should be set"
+            # This is the worst case scenario, we have to copy the bytes_data
+            # and can't just use the buffer directly. This is also very
+            # uncommon since we usually read the entire frame at once.
+            self._buffer += bytes_data
+        self._buffer_len += len(bytes_data)
+
+    def _remove_from_buffer(self) -> None:
+        """Remove data from the buffer."""
+        end_of_frame_pos = self._pos
+        self._buffer_len -= end_of_frame_pos
+        if self._buffer_len == 0:
+            # This is the best case scenario, we can just set the buffer to None
+            # and don't have to copy the data. This is the most common case as well.
+            self._buffer = None
+            return
+        if TYPE_CHECKING:
+            assert self._buffer is not None, "Buffer should be set"
+        # This is the worst case scenario, we have to copy the data
+        # and can't just use the buffer directly. This should only happen
+        # when we read multiple frames at once because the event loop
+        # is blocked and we cannot pull the data out of the buffer fast enough.
+        cstr = self._buffer
+        # Important: we must use the explicit length for the slice
+        # since Cython will stop at any '\0' character if we don't
+        self._buffer = cstr[end_of_frame_pos : self._buffer_len + end_of_frame_pos]
+
+    def _read(self, length: _int) -> bytes | None:
+        """Read exactly length bytes from the buffer or None if all the bytes are not yet available."""  # noqa: E501
+        new_pos = self._pos + length
+        if self._buffer_len < new_pos:
+            return None
+        original_pos = self._pos
+        self._pos = new_pos
+        if TYPE_CHECKING:
+            assert self._buffer is not None, "Buffer should be set"
+        cstr = self._buffer
+        # Important: we must keep the bounds check (self._buffer_len < new_pos)
+        # above to verify we never try to read past the end of the buffer
+        return cstr[original_pos:new_pos]
+
     def data_received(self, data: bytes) -> None:
         """Handle data received."""
-        try:
-            if (
-                self.future
-                and not self.future.done()
-                and (response := btmgmt_protocol.reader(data))
-                and response.cmd_response_frame
-            ):
-                self.future.set_result(response)
-        except ValueError as ex:
-            # ValueError: 47 is not a valid Events may happen on newer kernels
-            # and we need to ignore these events
-            _LOGGER.debug("Error parsing response: %s", ex)
-
-    async def send(self, *args: Any) -> btmgmt_protocol.Response:
-        """Send command."""
-        pkt_objs = btmgmt_protocol.command(*args)
-        self.future = self.loop.create_future()
-        if self.transport is None:
-            raise btmgmt_socket.BluetoothSocketError("Connection was closed")
-        self.transport.write(b"".join(frame.octets for frame in pkt_objs if frame))
-        cancel_timeout = self.loop.call_later(
-            self.timeout, self._timeout_future, self.future
-        )
-        try:
-            return await self.future
-        finally:
-            cancel_timeout.cancel()
-            self.future = None
+        self._add_to_buffer(data)
+        while self._buffer_len >= 6:
+            if TYPE_CHECKING:
+                assert self._buffer is not None, "Buffer should be set"
+            self._pos = 6
+            header = self._buffer
+            event_code = header[0] | (header[1] << 8)
+            controller_idx = header[2] | (header[3] << 8)
+            param_len = header[4] | (header[5] << 8)
+            print(
+                f"event_code: {event_code}, controller_idx: {controller_idx}, "
+                f"param_len: {param_len}"
+            )
+            self._read(param_len)
+            self._remove_from_buffer()
 
     def _timeout_future(self, future: asyncio.Future[btmgmt_protocol.Response]) -> None:
         if future and not future.done():
@@ -101,7 +147,7 @@ class MGMTBluetoothCtl:
                 # see https://bugs.python.org/issue38285
                 _, protocol = await loop._create_connection_transport(  # type: ignore[attr-defined]
                     self.sock,
-                    lambda: BluetoothMGMTProtocol(self.timeout, connection_made_future),
+                    lambda: BluetoothMGMTProtocol(connection_made_future),
                     None,
                     None,
                 )
