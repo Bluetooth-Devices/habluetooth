35322b3faa680bb1a5bb5fa90b37980b8b5d9ab8
J. Nick Koston <nick@koston.org>
2025-04-28 17:37:11 -0400

    example

--- examples/bluez_api.py
+++ examples/bluez_api.py
@@ -1,12 +1,37 @@
 import asyncio
 
+from habluetooth import BluetoothScanningMode
 from habluetooth.channels.bluez import MGMTBluetoothCtl
+from habluetooth.scanner import HaScanner
+
+int_ = int
+
+
+class LoggingHaScanner(HaScanner):
+    """Logging ha scanner."""
+
+    def _async_on_raw_bluez_advertisement(
+        self,
+        address: bytes,
+        address_type: int_,
+        rssi: int_,
+        flags: int_,
+        data: bytes,
+    ) -> None:
+        """Handle raw advertisement data."""
+        print(
+            f"address={address!r}, address_type={address_type}, "
+            f"rssi={rssi}, flags={flags}, data={data!r}"
+        )
 
 
 async def main() -> None:
     """Main function to test the Bluetooth management API."""
     # Create an instance of MGMTBluetoothCtl
-    mgmt_ctl = MGMTBluetoothCtl(timeout=5.0, scanners={})
+    scanner = LoggingHaScanner(
+        BluetoothScanningMode.ACTIVE, "hci0", "AA:BB:CC:DD:EE:FF"
+    )
+    mgmt_ctl = MGMTBluetoothCtl(timeout=5.0, scanners={0: scanner})
 
     # Set up the management interface
     await mgmt_ctl.setup()
