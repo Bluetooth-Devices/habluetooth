8760c4416188413f39227e8b21be425ab3d3dac5
J. Nick Koston <nick@koston.org>
2025-07-03 08:31:15 -0500

    Update src/habluetooth/scanner.py

--- src/habluetooth/scanner.py
+++ src/habluetooth/scanner.py
@@ -37,7 +37,6 @@ IS_LINUX = SYSTEM == "Linux"
 IS_MACOS = SYSTEM == "Darwin"
 
 if IS_LINUX:
-    # bleak>=1.0
     from bleak.args.bluez import BlueZScannerArgs, OrPattern
     from bleak.backends.bluezdbus.advertisement_monitor import (
         AdvertisementMonitor,
